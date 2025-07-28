63b5a22c0c35fe032b37e5751ce3becea1b24020
bblair321 <bblair321@yahoo.com>
2025-07-28 15:19:50 -0700

    all tests are passing

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
-
 from models import db, Article, User, ArticleSchema, UserSchema
 
 app = Flask(__name__)
@@ -12,7 +11,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 @app.route('/clear')
@@ -23,11 +21,22 @@ def clear_session():
 @app.route('/articles')
 def index_articles():
     articles = [ArticleSchema().dump(a) for a in Article.query.all()]
-    return make_response(articles)
+    return jsonify(articles), 200
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-    pass
+    if session.get('page_views', 0) >= 3:
+        return jsonify({'error': 'Maximum pageview limit reached'}), 401
+
+    article = Article.query.get(id)
+    if not article:
+        return jsonify({'error': 'Article not found'}), 404
+
+    session['page_views'] = session.get('page_views', 0) + 1
+
+    article_data = ArticleSchema().dump(article)
+    return jsonify(article_data), 200
+
 
 
 if __name__ == '__main__':
